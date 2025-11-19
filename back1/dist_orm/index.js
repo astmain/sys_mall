172019980bd2f8a5273e7691819771f6f6398e98
astmain <1311192345@qq.com>
2025-11-19 15:23:28 +0800

    1

---
+++ back1/dist_orm/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.my_add = my_add;
+function my_add(a, b) {
+    return a + b;
+}
+//# sourceMappingURL=index.js.map
--- back1/src/orm/index.ts
+++ back1/src/orm/index.ts
@@ -1,3 +1,3 @@
 export function my_add(a: number, b: number): number {
-    return a + b
+  return a + b
 }
