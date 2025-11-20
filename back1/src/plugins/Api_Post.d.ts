4733d055e6cbeb7872d9f1bd297dd81e2cbb8676
astmain <1311192345@qq.com>
2025-11-20 21:21:16 +0800

    1

--- back1/src/plugins/Api_Post.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function Api_Post(label: string, description?: string, Res_type?: any): (target: object, propertyKey: string, descriptor: PropertyDescriptor) => void;
--- back1/src/plugins/Api_group.d.ts
+++
@@ -1,4 +0,0 @@
-import 'reflect-metadata';
-export declare function Api_group(version: 'common' | 'v1' | 'v2' | 'test', name: string): <T extends {
-    new (...args: any[]): {};
-}>(target: T) => void;
