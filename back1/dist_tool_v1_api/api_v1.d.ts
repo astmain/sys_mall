df6afc2709393768330b0148dbb2a90ea95ebc27
astmain <1311192345@qq.com>
2025-11-20 17:51:24 +0800

    1

--- back1/dist_tool_v1_api/api_v1.d.ts
+++ back1/dist_tool_v1_api/api_v1.d.ts
@@ -1,4 +1,4 @@
-import type { login } from '@src/v1/auth/dto/login';
+import type { login } from '../v1/auth/dto/login';
 export declare let api_v1: {
     auth: {
         login: (form: InstanceType<typeof login>) => Promise<import("axios").AxiosResponse<any, any, {}>>;
