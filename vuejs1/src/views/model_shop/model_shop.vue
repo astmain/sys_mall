3b41dde264c5ffd77fd2bd2545efc850b6f06f92
astmain <1311192345@qq.com>
2025-11-21 15:46:00 +0800

    1

--- vuejs1/src/views/model_shop/model_shop.vue
+++ vuejs1/src/views/model_shop/model_shop.vue
@@ -6,13 +6,5 @@
 <script setup lang="tsx">
 import { ref } from "vue"
 import { ElMessage } from "element-plus"
-import * as tool_my from "tool_my"
-import type { tb_test1 } from "tool_typeorm"
 
-async function test1() {
-  ElMessage.success("test1")
-  console.log(`111---test1:`, 1111)
-  let one: tb_test1 = { name: "1111" }
-  console.log(`one---`, one)
-}
 </script>
